195cc5e1e265da92f756e75743bb65fc8338377b
liruzhi <1051810696@qq.com>
2022-09-23 19:09:37 +0800

    init

---
+++ src/views/product/tradeMark/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <!-- 按钮 -->
+    <el-button type="primary" icon="el-icon-plus">添加</el-button>
+    <!-- 表格组件 -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: "tradeMark",
+};
+</script>
+
+<style scoped></style>
