292b42b12ab1bff19522c85672f70e614e36d44b
liruzhi <1051810696@qq.com>
2022-09-27 20:04:01 +0800

    commit

--- src/api/product/tradeMark.js
+++ src/api/product/tradeMark.js
@@ -5,3 +5,23 @@ export const reqTradeMarkList = (page, limit) =>
     url: `/admin/product/baseTrademark/${page}/${limit}`,
     method: "get",
   });
+
+//添加品牌 /admin/product/baseTrademark/save  post
+
+//更新品牌  /admin/product/baseTrademark/update put
+
+export const reqAddOrUpdateTradeMark = (tradeMark) => {
+  if (tradeMark.id) {
+    return request({
+      url: `/admin/product/baseTrademark/save`,
+      method: "post",
+      data: tradeMark,
+    });
+  } else {
+    return request({
+      url: `/admin/product/baseTrademark/update`,
+      method: "put",
+      data: tradeMark,
+    });
+  }
+};
--- src/views/product/tradeMark/index.vue
+++ src/views/product/tradeMark/index.vue
@@ -1,7 +1,13 @@
 <template>
   <div>
     <!-- 按钮 -->
-    <el-button type="primary" icon="el-icon-plus">添加</el-button>
+    <el-button
+      type="primary"
+      icon="el-icon-plus"
+      style="margin: 10px 0"
+      @click="showDialog"
+      >添加</el-button
+    >
 
     <!-- 表格组件 -->
     <!-- type=index  展示序号 -->
@@ -17,7 +23,11 @@
       </el-table-column>
       <el-table-column prop="prop" label="操作" width="width">
         <template slot-scope="{ row }">
-          <el-button type="warning" icon="el-icon-edit" size="mini"
+          <el-button
+            type="warning"
+            icon="el-icon-edit"
+            size="mini"
+            @click="updateTradeMark(row)"
             >修改</el-button
           >
 
@@ -35,11 +45,48 @@
       :total="total"
       :page-size="limit"
       :pager-count="7"
-      :page-sizes="[3, 5, 10]"
-      layout="prev, pager, next, jumper"
-      @current-change="handleCurrentChange"
+      :page-sizes="[5, 10, 20]"
+      layout="prev, pager, next, jumper, ->, sizes ,total"
+      @current-change="getPageList"
+      @size-change="handleSizeChange"
     >
     </el-pagination>
+
+    <!-- 对话框 
+    :visible.sync 控制对话框显示与隐藏
+    -->
+    <el-dialog title="添加品牌" :visible.sync="dialogFormVisible">
+      <!-- form表单 -->
+      <el-form style="width: 80%" :model="tmForm">
+        <el-form-item label="品牌名称" label-width="100px">
+          <el-input autocomplete="off" v-model="tmForm.tmName"></el-input>
+        </el-form-item>
+        <el-form-item label="品牌Logo" label-width="100px">
+          <!-- 这里手机数据不能使用v-model，因为不是表单元素
+            action：设置图片上传的地址,路由需要增加dev-api前缀，这里注意下。
+           -->
+          <el-upload
+            class="avatar-uploader"
+            action="/dev-api/admin/product/fileUpload"
+            :show-file-list="false"
+            :on-success="handleAvatarSuccess"
+            :before-upload="beforeAvatarUpload"
+          >
+            <img v-if="tmForm.logoUrl" :src="tmForm.logoUrl" class="avatar" />
+            <i v-else class="el-icon-plus avatar-uploader-icon"></i>
+            <div slot="tip" class="el-upload__tip">
+              只能上传jpg/png文件，且不超过500kb
+            </div>
+          </el-upload>
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="dialogFormVisible = false">取 消</el-button>
+        <el-button type="primary" @click="addOrUpdateTradeMark"
+          >确 定</el-button
+        >
+      </div>
+    </el-dialog>
   </div>
 </template>
 
@@ -49,9 +96,16 @@ export default {
   data() {
     return {
       page: 1,
-      limit: 3,
+      limit: 5,
       total: 0,
       list: [],
+      //对话框显示与隐藏控制的属性
+      dialogFormVisible: false,
+      imageUrl: "",
+      tmForm: {
+        tmName: "",
+        logoUrl: "",
+      },
     };
   },
   //组件挂载完毕发请求
@@ -61,7 +115,8 @@ export default {
   },
   methods: {
     //获取品牌列表的数据
-    async getPageList() {
+    async getPageList(pager = 1) {
+      this.page = pager;
       //解构出参数
       const { page, limit } = this;
       //获取品牌列表的接口
@@ -69,17 +124,83 @@ export default {
       if (result.code == 200) {
         this.total = result.data.total;
         this.list = result.data.records;
-        this.page = result.data.current;
-        this.limit = result.data.size;
       }
     },
-    handleCurrentChange(pager) {
-      console.log(`当前页: ${pager}`);
-      this.page = pager;
+    handleSizeChange(limit) {
+      this.limit = limit;
       this.getPageList();
+    },
+    //点击添加
+    showDialog() {
+      this.dialogFormVisible = true;
+      this.tmForm = { tmName: "", logoUrl: "" };
+    },
+    //修改品牌
+    updateTradeMark(row) {
+      //row:当前用户选中的品牌信息
+      console.log(row);
+      this.dialogFormVisible = true;
+      this.tmForm = row;
+    },
+    handleAvatarSuccess(res, file) {
+      // this.imageUrl = URL.createObjectURL(file.raw);
+      //收集表单图片的数据，将来需要带给服务器
+      this.tmForm.logoUrl = res.data;
+    },
+    //图片上传之前
+    beforeAvatarUpload(file) {
+      const isJPG = file.type === "image/jpeg";
+      const isLt2M = file.size / 1024 / 1024 < 2;
+
+      if (!isJPG) {
+        this.$message.error("上传头像图片只能是 JPG 格式!");
+      }
+      if (!isLt2M) {
+        this.$message.error("上传头像图片大小不能超过 2MB!");
+      }
+      return isJPG && isLt2M;
+    },
+    //添加品牌或修改品牌
+    async addOrUpdateTradeMark() {
+      this.dialogFormVisible = false;
+      //发送请求
+      let result = await this.$API.trademark.reqAddOrUpdateTradeMark(
+        this.tmForm
+      );
+      console.log(result);
+      if (result.code == 200) {
+        //弹出信息
+        this.$message(this.tmForm.id ? "修改品牌成功" : "添加品牌成功");
+        this.getPageList();
+      }
     },
   },
 };
 </script>
 
-<style scoped></style>
+<style>
+/* 注意这里的style 不能加scoped 否则没有边框等 */
+.avatar-uploader .el-upload {
+  border: 1px dashed #d9d9d9;
+  border-radius: 6px;
+  cursor: pointer;
+  position: relative;
+  overflow: hidden;
+}
+.avatar-uploader .el-upload:hover {
+  border-color: #409eff;
+}
+.avatar-uploader-icon {
+  font-size: 28px;
+  color: #8c939d;
+  width: 178px;
+  height: 178px;
+  line-height: 178px;
+  text-align: center;
+}
+.avatar {
+  width: 178px;
+  height: 178px;
+  display: block;
+}
+</style>
